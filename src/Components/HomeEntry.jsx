7ecabf3e05b9bd82a812f933f31fe82b2bc4387f
David <bensadondavidn@gmai.com>
2025-07-16 04:46:40 +0300

    changes

--- src/Components/HomeEntry.jsx
+++ src/Components/HomeEntry.jsx
@@ -5,16 +5,17 @@ function HomeEntry() {
   return (
 
 <div className="home-entry-container">
-    <div className="home-entry">
-        <img src="/Images/logo-elsa-dan-home-entry.webp" alt="" className="logo-home-entry" />
-        <img src="Images/fleur-1.webp" className="fleur-1" />
+  <div className="fleurs">
+     <img src="Images/fleur-1.webp" className="fleur-1" />
         <img src="Images/fleur-2.webp" className="fleur-2" />
-
         <img src="Images/fleur-3.webp" className="fleur-4" />
         <img src="Images/fleur-5.webp" className="fleur-5" />
         <img src="Images/fleur-2.webp" className="fleur-6" />
         <img src="Images/fleur-3.webp" className="fleur-8" />
         <img src="Images/fleur-5.webp" className="fleur-9" />
+  </div>
+    <div className="home-entry">
+        <img src="/Images/logo-elsa-dan-home-entry.webp" alt="" className="logo-home-entry" />
     </div>
 </div>
   )
